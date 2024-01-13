5f69ef0a91832050c1f80bd4eba2e6d54bd90fbd
Milton <117595866+MiltonSandoval@users.noreply.github.com>
2024-01-12 21:54:07 -0400

    Add files via upload

---
+++ par.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int numero;
+	
+	cout<<"Ingrese el numero:";
+	cin>>numero;
+	if (numero % 2 == 0)
+	{
+		cout<<"El numero:"<<numero<<"es PAR"<<endl;
+	}
+	else
+	{
+	cout<<"el numero:"<<numero<<" es IMPAR" << endl;	
+	}
+	
+	
+	
+	return 0;
+}
+
