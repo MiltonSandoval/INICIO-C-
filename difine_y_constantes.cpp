1f611dd5ca370f861919a0a237f780474e58fbbd
Milton <117595866+MiltonSandoval@users.noreply.github.com>
2024-01-12 21:23:17 -0400

    Add files via upload

---
+++ difine_y_constantes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//#define PI 3.1416; //Definimos una constante llamada PI
+
+int main(int argc, char *argv[]) 
+{
+	const float PI = 3.1416; //Definimos una constante llamada PI
+	double diametro, circunferencia;
+	circunferencia=40;
+	diametro=circunferencia/PI;
+	cout<<"El diametro de la circunferencia de 40 es: " <<diametro;//<<endl;
+	//PI=0 //error por asignacion a la constante pi
+	return 0;
+}
+
