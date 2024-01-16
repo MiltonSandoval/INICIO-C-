4736638ce77e3c6c794af8f7365cebc79abd5ea3
Milton <117595866+MiltonSandoval@users.noreply.github.com>
2024-01-15 21:39:53 -0400

    Add files via upload

---
+++ vocales.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cctype>
+
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	char caracter; //CREAMOS UNA VARIABLE DE TIPO CHAR LLAMADA CARACTER
+	cout<<"Ingrese una letra:";
+	cin>>caracter; // SE ALMACENA EL CARACTER EN LA VARIABLE 'CARACTER'
+	caracter = tolower(caracter); //SE UTILIZA LA FUNCION TOLOWER PARA VOLVER EN MINUSCULA 
+	//LO QUE AlMACENA LA VARIABLE
+	switch(caracter)//switch CON OPCIONES PARA VER SI ES UNA VOCAL O NO 
+	{
+		case 'a':	
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u': cout<<"Es vocal";
+		break;
+		default: cout<<"No es vocal";
+		break;
+	};
+	return 0;
+}
+
