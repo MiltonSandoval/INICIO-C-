3f987d373d8cd02f6d4a2a74b5d03dea9c96d130
Milton <117595866+MiltonSandoval@users.noreply.github.com>
2024-01-17 21:46:38 -0400

    Update tabla_multiplicar.cpp

--- tabla_multiplicar.cpp
+++ tabla_multiplicar.cpp
@@ -14,8 +14,7 @@ int main(int argc, char *argv[])
 	int numero_tabla;//se crea la variable que almacenara la opcion del usuario
 	
 	cout<<"Tabla de multiplicar" "\n";
-	cout<<"Imprimir la tabla del:";//se pide al usuario ingresar el numero de la tabla que
-	//desea imprimir.
+	cout<<"Imprimir la tabla del:";//se pide al usuario ingresar el numero de la tabla que desea imprimir.
 	cin>>numero_tabla;//el numero se almacena en esta variable
 	/*
 	se inicializa el bucle for para empezar a imprimir la tabla hasta el 10,
