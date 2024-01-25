c3ca4879979adcd2d50e74325fe232efcc82249a
Milton <117595866+MiltonSandoval@users.noreply.github.com>
2024-01-24 21:50:54 -0400

    Add files via upload

---
+++ numero_impar_generador.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <random>
+#include <limits> // Para std:: numeric limits
+
+using namespace std;
+//Declaracion de variables globales.
+int aleatorio, num1, contador;
+//Declaracion de Funciones.
+void solicitarNumero()//Funcion void que sirve para pedir  un numeros y almacenarlos en la variable "aleatorio"
+{
+	cout<<"Ingrese la cantidad de numeros impares a generar:";
+	cin>>aleatorio;
+}
+//Funcion para generar un numero aleatorio entre 0 a 99.
+int generador()
+{
+	static random_device rd; //dispositivo para obtener una semilla aleatoria.
+	static mt19937 gen(rd()); //motor de generador Mersenne Twister
+	uniform_int_distribution<> distrib(0,99);
+	distrib(gen);
+	return distrib(gen);
+}
+//funcion que sirve para verificar que el numero es impar.
+bool verificador(int numero)
+{
+	return ( numero % 2 != 0 );
+}
+
+int main()
+{
+	//se solicita el numero.
+	solicitarNumero();
+	while (contador < aleatorio)
+	{
+		//se guarda ell numero generado en la variable num1
+		num1 = generador();
+		//si la funcion verificador devuelve 1, se imprime el numero impar
+		if(verificador(num1)) //se verifica que el numera sea impar.
+		{
+			cout<<"Impar:"<<num1<<endl;//se imprime el numero.
+			contador = contador + 1;
+		}
+	}
+	return 0;
+}
---
+++ sin_titulo.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+void ingresar(int &a, int &b)
+{
+	cout<<"Ingresar un numero:";
+	cin>>a;
+	cout<<"Ingresar otro numero:";
+	cin>>b;
+
+}
+
+void sumar(int a, int b , int &resultado)
+{
+	resultado = a+b;
+}
+
+void primo(int a, string &resultadoA)
+{
+	cout<<"Ingrese un numero:";
+	cin>>a;
+	if ((a % a == 0) and (a % 1 == 0) and (a % a+1 == 0))
+	{
+		resultadoA = "ESTE NUMERO ES UN NUMERO PRIMO", a;
+		cout<<resultadoA;
+	}
+	
+}
+void menuopcion(int opcion, int a, int b, int &resultado)
+{
+	
+	
+}
+
+int main(int argc, char *argv[]) {
+	int a,b,resultado,opcion;
+	string resultadoA;
+	do
+	{
+		cout<<"(1).INGRESAR NUMEROS"<<"\n";
+		cout<<"(2).SUMAR LOS NUMEROS"<<"\n";
+		cout<<"(3).VERIFICAR SI LOS NUMEROS SON PRIMOS"<<"\n";
+		cout<<"(0).SALIR";
+		primo(a, resultadoA);
+		
+	
+	}while(opcion == 0);
+	
+	return 0;
+}
+
